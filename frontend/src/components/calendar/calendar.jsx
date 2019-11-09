a38adfc5fd5290516951eecd4c0ab1f36cefd6bd
Davor Ergotic <dergotic@PNIM-C02YK151JGH6.local>
2019-11-08 16:14:15 -0800

    Cleanup

--- frontend/src/components/calendar/calendar.jsx
+++ frontend/src/components/calendar/calendar.jsx
@@ -1,108 +1,99 @@
-import React from "react";
-import "./styles.scss";
+import React from 'react'
+import DayNames from './dayNames'
+import './styles.scss'
 
 class Calendar extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       events: [],
       showPendingRegistrations: false,
-      eventStatus: ""
-    };
+      eventStatus: '',
+    }
   }
 
   componentDidMount() {
-    fetch(
-      "https://demo14.secure.retreat.guru/api/v1/registrations?token=ef061e1a717568ee5ca5c76a94cf5842"
-    )
+    fetch('https://demo14.secure.retreat.guru/api/v1/registrations?token=ef061e1a717568ee5ca5c76a94cf5842')
       .then(res => res.json())
       .then(
         result => {
-          this.setState({
-            events: result
-          });
+          this.setState({ events: result })
         },
         error => {
-          console.log("handle error");
+          console.log('handle error')
         }
-      );
+      )
   }
 
   renderDays = () => {
-    const numberOfDays = new Date(2025, 8, 0).getDate();
-    let days = [];
+    const events = this.state.events
+    const numberOfDays = new Date(2025, 8, 0).getDate()
 
-    const events = this.state.events;
-
-    let freeDays = [];
-    let ocupiedDays = [];
+    let days = []
+    let freeDays = []
+    let ocupiedDays = []
 
     for (let i = 1; i <= numberOfDays; i++) {
       for (let j = 0; j < events.length; j++) {
-        if (events[j].start_date === "2025-08-" + i) {
+        if (events[j].start_date === '2025-08-' + x) {
           ocupiedDays.push({
-            date: new Date("2025-08-" + i),
+            date: new Date('2025-08-' + x),
             start_date: events[j].start_date,
             end_date: events[j].end_date,
             full_name: events[j].full_name,
             status: events[j].status,
-            room_id: events[j].room_id
-          });
-          break;
+            room_id: events[j].room_id,
+          })
+          break
         }
       }
 
       freeDays.push({
-        date: new Date("2025-08-" + i),
-        start_date: "",
-        end_date: "",
-        full_name: "",
-        status: "",
-        room_id: ""
-      });
+        date: new Date('2025-08-' + x),
+        start_date: '',
+        end_date: '',
+        full_name: '',
+        status: '',
+        room_id: '',
+      })
     }
 
-    const allDays = freeDays.concat(ocupiedDays);
+    const allDays = freeDays.concat(ocupiedDays)
 
     for (let i = 0; i < allDays.length; i++) {
       for (var j = i + 1; j < allDays.length; j++) {
         if (allDays[i].date.toString() === allDays[j].date.toString()) {
-          allDays.splice(i--, 1);
+          allDays.splice(i--, 1)
         }
       }
     }
 
-    let byDate = allDays.slice(0);
+    let byDate = allDays.slice(0)
     byDate.sort((a, b) => {
-      return a.date - b.date;
-    });
+      return a.date - b.date
+    })
 
-    let className;
-
-    console.log('by date: ',byDate);
+    let className
 
     for (let i = 0; i < byDate.length; i++) {
-      if (byDate[i].start_date !== "" && byDate[i].status === "reserved") {
-        className = "reserved";
+      if (byDate[i].start_date !== '' && byDate[i].status === 'reserved') {
+        className = 'reserved'
       } else if (
-        byDate[i].start_date !== "" &&
+        byDate[i].start_date !== '' &&
         this.state.showPendingRegistrations === true &&
-        byDate[i].status === "pending"
+        byDate[i].status === 'pending'
       ) {
-        className = "pending";
+        className = 'pending'
       } else {
-        className = "";
+        className = ''
       }
 
       days.push(
-        <button key={i} className={className + " tooltip"}>
+        <button key={i} className={className + ' tooltip'}>
           {i + 1}
-          <time
-            dateTime={"2025-08-" + i + 1}
-            className={byDate[i].start_date ? "tooltiptext" : ""}
-          >
-            <span style={{ display: className ? "block" : "none" }}>
+          <time dateTime={'2025-08-' + i + 1} className={byDate[i].start_date ? 'tooltiptext' : ''}>
+            <span style={{ display: className ? 'block' : 'none' }}>
               <strong>Start Date: </strong> {byDate[i].start_date} <br />
               <strong>End Date:</strong> {byDate[i].end_date} <br />
               <strong>Name: </strong> {byDate[i].full_name} <br />
@@ -110,53 +101,41 @@ class Calendar extends React.Component {
             </span>
           </time>
         </button>
-      );
+      )
     }
-    return days;
-  };
+    return days
+  }
 
   handleChangeChk = () => {
     if (this.state.showPendingRegistrations) {
       this.setState({
-        showPendingRegistrations: false
-      });
+        showPendingRegistrations: false,
+      })
     } else {
       this.setState({
-        showPendingRegistrations: true
-      });
+        showPendingRegistrations: true,
+      })
     }
-  };
+  }
 
   render() {
-    const { showPendingRegistrations } = this.state;
+    const { showPendingRegistrations } = this.state
     return (
       <main>
         <label>
-          <input
-            type="checkbox"
-            defaultChecked={showPendingRegistrations}
-            onChange={this.handleChangeChk}
-          />
+          <input type="checkbox" defaultChecked={showPendingRegistrations} onChange={this.handleChangeChk} />
           Show Pending Registrations
         </label>
         <div className="calendar">
           <div className="month-indicator">
-            <time dateTime="2025-08"> August 2025 </time>
+            <time dateTime="2025-08">August 2025 </time>
           </div>
-          <div className="day-of-week">
-            <div>Su</div>
-            <div>Mo</div>
-            <div>Tu</div>
-            <div>We</div>
-            <div>Th</div>
-            <div>Fr</div>
-            <div>Sa</div>
-          </div>
+          <DayNames />
           <div className="date-grid">{this.renderDays()}</div>
         </div>
       </main>
-    );
+    )
   }
 }
 
-export default Calendar;
+export default Calendar
--- frontend/src/components/calendar/dayNames.jsx
+++ frontend/src/components/calendar/dayNames.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default () => {
   return (
-    <div className="row day-names">
-      <span className="day">Sun</span>
-      <span className="day">Mon</span>
-      <span className="day">Tue</span>
-      <span className="day">Wed</span>
-      <span className="day">Thu</span>
-      <span className="day">Fri</span>
-      <span className="day">Sat</span>
+    <div className="day-of-week">
+      <div>Su</div>
+      <div>Mo</div>
+      <div>Tu</div>
+      <div>We</div>
+      <div>Th</div>
+      <div>Fr</div>
+      <div>Sa</div>
     </div>
   );
 };
